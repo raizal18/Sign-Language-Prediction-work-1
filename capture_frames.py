1430d47b63b444f4f0651fff69159ab1e510f392
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-04-25 16:54:49 +0530

    none

--- capture_frames.py
+++ capture_frames.py
@@ -73,96 +73,96 @@ video_labelled = pd.DataFrame(list(zip(file_url,class_description)),columns=["vi
 
 
 
-# def get_feature(sample,sample_label):
-#     mp_holistic = mp.solutions.holistic # Holistic model
-#     mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+def get_feature(sample,sample_label):
+    mp_holistic = mp.solutions.holistic # Holistic model
+    mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
 
-#     def mediapipe_detection(image, model):
+    def mediapipe_detection(image, model):
     
-#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-#         image.flags.writeable = False                  # Image is no longer writeable
-#         results = model.process(image)                 # Make prediction
-#         image.flags.writeable = True                   # Image is now writeable 
-#         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
-#         return image, results
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+        image.flags.writeable = False                  # Image is no longer writeable
+        results = model.process(image)                 # Make prediction
+        image.flags.writeable = True                   # Image is now writeable 
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+        return image, results
 
 
-#     def draw_styled_landmarks(image, results):
-#         # Draw face connections
-#         mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
-#                                 mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-#                                 mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-#                                 ) 
-#         # Draw pose connections
-#         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-#                                 mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-#                                 mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-#                                 ) 
-#         # Draw left hand connections
-#         mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-#                                 mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
-#                                 mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-#                                 ) 
-#         # Draw right hand connections  
-#         mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-#                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
-#                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-#                                 ) 
+    def draw_styled_landmarks(image, results):
+        # Draw face connections
+        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
+                                mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                                mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                                ) 
+        # Draw pose connections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                                mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                                ) 
+        # Draw left hand connections
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                                mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                                ) 
+        # Draw right hand connections  
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                ) 
 
-#     cap = cv2.VideoCapture(sample)
+    cap = cv2.VideoCapture(sample)
 
-#     # Set mediapipe model 
-#     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-#         while cap.isOpened():
+    # Set mediapipe model 
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
 
-#             # Read feed
-#             ret, frame = cap.read()
-#             if not ret:
+            # Read feed
+            ret, frame = cap.read()
+            if not ret:
 
-#                 print("Can't receive frame (stream end?). Exiting ...")
-#                 cap.release()
-#                 cv2.destroyAllWindows()
-#                 break
-#             # Make detections
-#             frame = cv2.resize(frame, (720, 480),interpolation = cv2.INTER_LINEAR)
-#             image, results = mediapipe_detection(frame, holistic)
-#             print(results)
+                print("Can't receive frame (stream end?). Exiting ...")
+                cap.release()
+                cv2.destroyAllWindows()
+                break
+            # Make detections
+            frame = cv2.resize(frame, (720, 480),interpolation = cv2.INTER_LINEAR)
+            image, results = mediapipe_detection(frame, holistic)
+            print(results)
             
-#             # Draw landmarks
-#             draw_styled_landmarks(image, results)
-#             # Show to screen
+            # Draw landmarks
+            draw_styled_landmarks(image, results)
+            # Show to screen
 
-#             org = (5, 40)
+            org = (5, 40)
             
-#             # fontScale
-#             fontScale = 1
+            # fontScale
+            fontScale = 1
             
-#             # Blue color in BGR
-#             color = (38, 249, 255)
+            # Blue color in BGR
+            color = (38, 249, 255)
             
-#             # Line thickness of 2 px
-#             thickness = 1
+            # Line thickness of 2 px
+            thickness = 1
             
-#             # Using cv2.putText() method
-#             font = cv2.FONT_HERSHEY_COMPLEX
-#             image = cv2.putText(image,f"Label: {sample_label}", org, font, 
-#                             fontScale, color, thickness, cv2.LINE_AA)
+            # Using cv2.putText() method
+            font = cv2.FONT_HERSHEY_COMPLEX
+            image = cv2.putText(image,f"Label: {sample_label}", org, font, 
+                            fontScale, color, thickness, cv2.LINE_AA)
     
-#             cv2.imshow('Joint Position & action ', image)
+            cv2.imshow('Joint Position & action ', image)
 
-#             # Break gracefully
-#             if cv2.waitKey(10) & 0xFF == ord('q'):
-#                 break
-#         cap.release()
-#         cv2.destroyAllWindows()
+            # Break gracefully
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 
-# sample = video_labelled['video location'].values[17]
-# sample_label = video_labelled['label'].values[17]
+sample = video_labelled['video location'].values[17]
+sample_label = video_labelled['label'].values[17]
 
-# get_feature(sample,sample_label)
+get_feature(sample,sample_label)
 
 
 
