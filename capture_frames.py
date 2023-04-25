30790b013c5830ba7527b5c64029379dde79922e
raizal18 <ramakrishnanmurugan6@gmail.com>
2023-04-25 18:51:11 +0530

    ddddd

--- capture_frames.py
+++ capture_frames.py
@@ -165,7 +165,7 @@ sample_label = video_labelled['label'].values[17]
 get_feature(sample,sample_label)
 
 EXTRACT_FEATURE =False
-if EXTRACT_FEATURE == True
+if EXTRACT_FEATURE == True:
     def extract_keypoints(results):
         pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
         face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
